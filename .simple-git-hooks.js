22e509cd827345b1ca3530331d5a7f9d105b9e6a
iskl <sunkeliang@gmail.com>
2023-08-08 15:46:24 +0800

    chore: npx

--- .simple-git-hooks.js
+++ .simple-git-hooks.js
@@ -5,5 +5,5 @@
 module.exports = {
   // 'prepare-commit-msg': 'exec < /dev/tty && npx cz --hook || true',
   'pre-commit': 'npx lint-staged',
-  'commit-msg': 'pnpm exec tsx scripts/verifyCommit.ts $1',
+  'commit-msg': 'tsx scripts/verifyCommit.ts $1',
 };
