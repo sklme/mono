88359903b7fb4eef3aeea690269d011d109db3d2
iskl <sunkeliang@gmail.com>
2023-08-08 20:35:02 +0800

    feat: global prefix

---
+++ apps/server/src/constants/env.ts
@@ -0,0 +1,2 @@
+// 一些整体的环境变量
+export const GLOBAL_PREFIX = 'api/';
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GLOBAL_PREFIX } from 'src/constants/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局路径前缀
+  app.setGlobalPrefix(GLOBAL_PREFIX);
+
   await app.listen(3000);
 }
 bootstrap();
