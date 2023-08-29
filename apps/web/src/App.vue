154ca28d9891813a905fc55a01456b0fc7997a12
iskl <sunkeliang@gmail.com>
2023-08-29 16:41:21 +0800

    feat: page

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -4,17 +4,4 @@
 
 <script setup lang="ts"></script>
 
-<style lang="less">
-:root {
-  min-width: 200px;
-  max-width: 980px;
-  margin: 0 auto;
-  padding: 45px;
-}
-
-@media (max-width: 767px) {
-  :root {
-    padding: 15px;
-  }
-}
-</style>
+<style lang="less"></style>
---
+++ apps/web/src/Pages/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>哈哈</div>
+</template>
+
+<script setup lang="ts">
+import BaseService from '~/service/base';
+
+//
+
+const s = new BaseService();
+
+const x = s.post({
+  url: 'test',
+});
+
+void x.then((d) => {
+  console.log(d);
+});
+</script>
+
+<style lang="less" scoped>
+//
+</style>
