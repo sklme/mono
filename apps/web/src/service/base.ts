faf89520f6e9e36b5e2f7a8897242f93ef71e10c
iskl <sunkeliang@gmail.com>
2023-08-29 16:40:28 +0800

    feat: base service

--- apps/web/src/service/base.ts
+++ apps/web/src/service/base.ts
@@ -19,11 +19,29 @@ export class BaseService {
     this.axios = axios.create({
       baseURL,
     });
+
+    // 个月axios增加一个错误处理的拦截器
+    this.axios.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      // (error) => {
+      //   // 错误处理
+      //   return Promise.reject(error);
+      // }
+    );
   }
 
   get<T>(config: AxiosRequestConfig) {
     return this.axios.request<HttpResponse<T>>({
       method: 'GET',
+      ...config,
+    });
+  }
+
+  post<T>(config: AxiosRequestConfig) {
+    return this.axios.request<HttpResponse<T>>({
+      method: 'POST',
       ...config,
     });
   }
