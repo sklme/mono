6898e38def08b7089c64efd130263652f180e36d
iskl <sunkeliang@gmail.com>
2023-08-29 16:38:32 +0800

    chore: route

--- apps/web/src/router/routes.ts
+++ apps/web/src/router/routes.ts
@@ -1,40 +1,12 @@
 import { RouteRecordRaw } from 'vue-router';
-import BuildFeatVue from '~/components/preset/feat/BuildFeat.vue';
-import CriteriaFeatVue from '~/components/preset/feat/CriteriaFeat.vue';
-import LogicFeatVue from '~/components/preset/feat/LogicFeat.vue';
-import FeatHomeVue from '~/components/preset/FeatHome.vue';
-
-const feature: RouteRecordRaw = {
-  path: '/feature',
-  name: 'Feature',
-  redirect: () => {
-    return { name: 'BuildFeat' };
-  },
-  children: [
-    {
-      path: 'build_dev_test_deploy',
-      name: 'BuildFeat',
-      component: BuildFeatVue,
-    },
-    {
-      path: 'creteria',
-      name: 'Creteria',
-      component: CriteriaFeatVue,
-    },
-    {
-      path: 'logic',
-      name: 'Logic',
-      component: LogicFeatVue,
-    },
-  ],
-};
+import HomePage from '~/Pages/HomePage.vue';
 
 export const routes: RouteRecordRaw[] = [
+  // #region 主页
   {
     path: '/',
     name: 'Index',
-    component: FeatHomeVue,
+    component: HomePage,
   },
-  // 功能
-  feature,
+  // #endregion 主页
 ];
